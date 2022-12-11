f0a2f501cabf3bce4883a723a02791c0305902ef
CSJ <tmdwns9630@gmail.com>
2022-12-12 04:04:28 +0900

    일단 html로 템플릿 정도는 만들었고, d3.js를 사용한 차트를 넣기만 해봄

---
+++ VSCode/d3dj-piechart.js
@@ -0,0 +1,48 @@
+const svgDimensions = {
+  width: 300,
+  height: 300,
+};
+const radius = Math.min(svgDimensions.width, svgDimensions.height) / 2;
+const data = [620, 80, 130, 210, 510, 80];
+
+// const svg = d3
+//   .select("body")
+//   .append("svg")
+//   .attr("width", svgDimensions.width)
+//   .attr("height", svgDimensions.height)
+//   .style("border", "1px solid rgba(0,0,0,0.1)");
+
+const svg = d3
+  .select("svg")
+  .append("g")
+  .attr("width", svgDimensions.width)
+  .attr("height", svgDimensions.height)
+  .style("border", "1px solid rgba(0,0,0,0.1)");
+
+const g = svg
+  .append("g")
+  .attr(
+    "transform",
+    `translate(${svgDimensions.width / 2}, ${svgDimensions.height / 2})`
+  );
+const color = d3.scaleOrdinal([
+  "#ff9800",
+  "#ffa726",
+  "#ffb74d",
+  "#ffcc80",
+  "#ffe0b2",
+  "#fff3e0",
+]);
+
+const pie = d3.pie();
+const arc = d3.arc().innerRadius(0).outerRadius(radius);
+const arcs = g
+  .selectAll("arc")
+  .data(pie(data))
+  .enter()
+  .append("g")
+  .attr("class", "arc");
+arcs
+  .append("path")
+  .attr("fill", (d, i) => color(i))
+  .attr("d", arc);
